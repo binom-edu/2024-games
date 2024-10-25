6b9b30a7973df88e58343e4fe46c0ffadf398540
Alexander Klindyuk <info@binom-r.ru>
2024-10-25 20:19:43 +0300

    занятие 5

--- gallows/game.py
+++ gallows/game.py
@@ -1,3 +1,5 @@
+import random, os
+
 frames = [
 '''
 -------||
@@ -63,4 +65,32 @@ frames = [
 ==========
 ''',
 ]
-print(frames[5])
+
+def print_status():
+    '''Выводит на экран состояние игры'''
+    print('Привет!')
+
+def get_user_input():
+    '''Получает ход игрока с проверкой корректности'''
+    pass
+
+def check_move():
+    '''Проверяет, отгадана ли буква'''
+    pass
+
+def check_win():
+    '''Проверка победы игрока'''
+    pass
+
+def check_lose():
+    '''Проверка поражения игрока'''
+    pass
+
+filedir = os.path.dirname(__file__)
+fin = open(os.path.join(filedir, 'words.txt'), 'r', encoding='utf8')
+words = fin.read().splitlines()
+secret = random.choice(words)
+print(secret)
+wrong = []
+correct = []
+print_status()
