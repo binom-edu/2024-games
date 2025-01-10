9211d5368ce598c671fe3d56dbdad4092f9100f2
Alexander Klindyuk <info@binom-r.ru>
2025-01-10 20:08:18 +0300

    занятие 13

--- locator/game.py
+++ locator/game.py
@@ -16,12 +16,38 @@ def make_board(width: int, height: int) -> list:
         res.append(line)
     return res
 
-def print_board(board: list) -> None:
-    for line in board:
-        print(*line)
+def print_board(board: list, debug_mode=False) -> None:
+    if debug_mode:
+        for i, j in chests:
+            board[i][j] = PRIZE
+    print('   ', end='')
+    for x in range(WIDTH):
+        if x % 10 == 0:
+            print(x // 10, end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+    print('   ', end='')
+    for x in range(WIDTH):
+        print(x % 10, end=' ')
+    print()
+    for i in range(HEIGHT):
+        print(str(i).rjust(2, ' '), *board[i])
+
+def make_chests(cnt: int) -> list:
+    '''Прячет на карте cnt сундуков'''
+    chests = []
+    while len(chests) < cnt:
+        chest = (random.randint(0, HEIGHT - 1), random.randint(0, WIDTH - 1))
+        if chest not in chests:
+            chests.append(chest)
+    return chests
 
 WIDTH = 30
 HEIGHT = 10
+PRIZE = '☠'
 
 board = make_board(WIDTH, HEIGHT)
-print_board(board)
+chests = make_chests(3)
+# print(chests)
+print_board(board, debug_mode=True)
