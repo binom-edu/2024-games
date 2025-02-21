37c9afbb44c57d01d6f073638d4ec1dcf966b844
Klindyuk <klindyuk@gmail.com>
2025-02-21 20:16:15 +0300

    19

--- spaceshooter/game.py
+++ spaceshooter/game.py
@@ -20,6 +20,38 @@ class Hero(pygame.sprite.Sprite):
             if self.rect.left < 0:
                 self.rect.left = 0
 
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = random.choice(['small', 'middle', 'big'])
+        path = os.path.join(IMG_DIR, 'mobs', self.size)
+        filenames = os.listdir(path)
+        self.image = pygame.image.load(
+            os.path.join(path, random.choice(filenames))
+        ).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = (
+            random.randint(-100, WIDTH + 100),
+            random.randint(-400, -100)
+        )
+        self.vx = random.randint(-2, 2)
+        self.vy = random.randint(1, 5)
+        self.vr = random.randint(-3, 3)
+        self.angle = 0
+        all_sprites.add(self)
+        mobs.add(self)
+    
+    def update(self):
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+        if self.rect.top > SCENE_HEIGHT:
+            self.rect.center = (
+                random.randint(-100, WIDTH + 100),
+                random.randint(-400, -100)
+            )
+        # self.angle = (self.angle + self.vr) % 360
+        # self.image = pygame.transform.rotate(self.image, self.angle)
+
 pygame.init()
 WIDTH = 480
 HEIGHT = 640
@@ -35,7 +67,10 @@ info = pygame.surface.Surface((WIDTH, INFO_HEIGHT))
 scene = pygame.surface.Surface((WIDTH, SCENE_HEIGHT))
 
 all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
 hero = Hero()
+for i in range(10):
+    Mob()
 
 clock = pygame.time.Clock()
 
